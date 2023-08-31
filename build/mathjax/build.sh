9058a44b0fb2d57d564cd3ac59b296aa56033126
Esscraye <esscrayebat@netcourrier.com>
2023-08-31 21:28:58 +0200

    change(git clone): in order to stay up to date

--- build/mathjax/build.sh
+++ build/mathjax/build.sh
@@ -3,7 +3,7 @@
 # set current working directory to directory of the shell script
 cd "$(dirname "$0")"
 
-git clone --depth 1 --branch 3.2.2 https://github.com/mathjax/MathJax.git tmp
+git clone https://github.com/mathjax/MathJax.git tmp
 
 # https://github.com/mathjax/MathJax#reducing-the-size-of-the-components-directory
 mkdir -p ../../vendor/mathjax
