ff1a0f463b6dc89af6b3bc2f1febc8abb1988293
Esscraye <esscrayebat@netcourrier.com>
2023-09-01 14:10:17 +0200

    update version of mithril

--- build/mithril/build.sh
+++ build/mithril/build.sh
@@ -3,4 +3,4 @@
 # set current working directory to directory of the shell script
 cd "$(dirname "$0")"
 
-curl https://cdnjs.cloudflare.com/ajax/libs/mithril/1.1.6/mithril.min.js --output ../../vendor/mithril.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/mithril/2.2.3/mithril.min.js --output ../../vendor/mithril.min.js
